02ab711869051f8e5cb8e2e82cc4f9193249428d
gayeon7877 <97147501+gayeon7877@users.noreply.github.com>
2022-06-27 14:31:48 +0900

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,12 @@
+N,M=map(int,input().split())
+
+result=0
+
+for i in range(N):
+    data=list(map(int,input().split()))
+    min_value=10001
+    for a in data:
+        min_value=min(min_value,a)
+    result=max(result,min_value)
+
+print(result)
