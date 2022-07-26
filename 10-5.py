48388831113925f17cff46474383b6fa87e25b3a
gayeon7877 <97147501+gayeon7877@users.noreply.github.com>
2022-07-26 19:59:13 +0900

    Add files via upload

---
+++ 10-5.py
@@ -0,0 +1,29 @@
+n,m=map(int,input().split())
+
+parent=[0]*(n+1)
+
+for i in range(0,n+1):
+    parent[i]=i
+
+def find_parent(parent,x):
+   if parent[x]!=x:
+       parent[x]=find_parent(parent,parent[x])
+   return parent[x]
+
+def union_parent(parent,a,b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
+
+for _ in range(m):
+    a,b,c=map(int,input().split())
+    if a==0:
+        union_parent(parent,a,b)
+    else:
+       if find_parent(parent,b)==find_parent(parent,c):
+           print('YES')
+       else:
+           print('NO')
